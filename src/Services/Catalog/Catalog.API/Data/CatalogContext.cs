772b510ecbfa31579b7aebd872f8e0d0b1588d4b
Sohail Arshad <rmsohaila@gmail.com>
2023-02-12 01:54:37 +0500

    Catalog API added with mongo

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.API.Data.Interfaces;
+using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration config)
+        {
+            var client = new MongoClient(config.GetValue<string>("ConnectionStrings:MongoConnectionString"));
+            var database = client.GetDatabase(config.GetValue<string>("ConnectionStrings:DatabaseName"));
+            Products = database.GetCollection<Product>(config.GetValue<string>("ConnectionStrings:ProductCollectionName"));
+            CatalogSeeder.SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using Catalog.API.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IList<Product>> GetAll();
+        Task<Product> GetById(string id);
+        Task<IList<Product>> GetByName(string name);
+        Task<IList<Product>> GetByCategory(string category);
+
+        Task Create(Product product);
+        Task<bool> Update(Product product);
+        Task<bool> Delete(string id);
+    }
+}
