fbd0b18c3ea1d5accd8305a19fc9cc5faf1adaa0
Sohail Arshad <rmsohaila@gmail.com>
2023-08-30 22:42:10 +0500

    ordering init

---
+++ src/Services/Ordering/Ordering.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -0,0 +1,17 @@
+﻿using Ordering.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Contracts.Persistence
+{
+    public interface IAsyncRepository<T> where T : BaseEntity
+    {
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate);
+        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null,
+                                        Func<IQueryable<T>>);
+    }
+}
